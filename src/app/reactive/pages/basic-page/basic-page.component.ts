7de8d39bf247079aaf33f86de641e6552a2d60d3
TitoCalleros <mirna.molina@gmail.com>
2025-03-19 12:15:17 -0600

    Creación de funciones para obtener los errores de cada elemento del formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -17,10 +17,35 @@ export class BasicPageComponent {
 
   private fb  = inject(FormBuilder);
 
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     name: ['',  /** Validadores síncronos:  */ [Validators.required, Validators.minLength(3)],  /** validadores asíncronos */ [] ],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]]
-  })
+  });
+
+  isValidField( fieldName: string ): boolean | null {
+    return !!this.myForm.controls[fieldName].errors;
+  }
+
+  getFieldError( fieldName: string ): string | null {
+    if ( !this.myForm.controls[fieldName] ) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo de ${ errors['minlength'].requiredLength } caracteres`;
+
+        case 'min':
+          return `Valor mínimo de ${errors['min'].min }`;
+      }
+    }
+
+    return null;
+  }
 
 }
