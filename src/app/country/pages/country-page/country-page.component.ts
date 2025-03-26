dfc69cdeea6b73c4c3934c1e3c4c6f5dcd103d45
TitoCalleros <mirna.molina@gmail.com>
2025-03-25 19:26:06 -0600

    Declaración del formulario y de las señales a utilizar para detectar cambios en el formulario.

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { CountryService } from '../../services/country.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { Country } from '../../interfaces/country.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+
+  fb = inject(FormBuilder);
+
+  countryService = inject(CountryService);
+
+  regions = signal(this.countryService.regions);
+
+  countriesByRegion = signal<Country[]>([]);
+  countriesBorders = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+  onFormChanged = effect( ( onCleanup ) => {
+    const regionSubscription = this.onRegionChanged();
+
+    onCleanup( () => {
+      regionSubscription?.unsubscribe();
+    })
+  });
+
+  onRegionChanged() {
+    return this.myForm.get('region')!.valueChanges.pipe(
+      tap( () => this.myForm.get('country')!.setValue('')),
+      tap( () => this.myForm.get('border')!.setValue('')),
+      tap( () => {
+        this.countriesByRegion.set([]);
+        this.countriesBorders.set([]);
+      }),
+      switchMap(region => this.countryService.getCountriesByRegion(region!))
+    ). subscribe( countries => {
+      this.countriesByRegion.set(countries);
+    });
+  }
+
+
+}
